02dfa93deb876b908e9061ed490e5ec6c548b16a
ahmad dhohir azhari <dhohir12@gmail.com>
2022-09-02 21:24:11 +0700

    submit target friday

--- src/controllers/product.js
+++ src/controllers/product.js
@@ -1,9 +1,108 @@
+const { request } = require("express");
+const productModel = require("../models/product");
+const wrapper = require("../utils/wrapper");
+
 module.exports = {
   showGreetings: async (request, response) => {
     try {
-      response.status(200).send("Hello World!");
+      // return response.status(200).send("Hello World!");
+      return wrapper.response(
+        response,
+        200,
+        "Success Get Greetings",
+        "Hello World !"
+      );
     } catch (error) {
-      console.log(error);
+      const {
+        status = 500,
+        statusText = "Internal Server Error",
+        error: errorData = null,
+      } = error;
+      return wrapper.response(response, status, statusText, errorData);
+    }
+  },
+  getAllProduct: async (request, response) => {
+    try {
+      console.log(request.query);
+      const result = await productModel.getAllProduct();
+      return wrapper.response(
+        response,
+        result.status,
+        "Success Get Data !",
+        result.data
+      );
+    } catch (error) {
+      const {
+        status = 500,
+        statusText = "Internal Server Error",
+        error: errorData = null,
+      } = error;
+      return wrapper.response(response, status, statusText, errorData);
+    }
+  },
+  getProductById: async (request, response) => {
+    try {
+      // const request = {
+      //   // ...
+      //   params: { id: "12345678" },
+      //   // ...
+      // };
+      const { id } = request.params;
+
+      const result = await productModel.getProductById(id);
+
+      if (result.data.length < 1) {
+        return wrapper.response(
+          response,
+          404,
+          `Data By Id ${id} Not Found`,
+          []
+        );
+      }
+
+      return wrapper.response(
+        response,
+        result.status,
+        "Success Get Data By Id",
+        result.data
+      );
+    } catch (error) {
+      const {
+        status = 500,
+        statusText = "Internal Server Error",
+        error: errorData = null,
+      } = error;
+      return wrapper.response(response, status, statusText, errorData);
+    }
+  },
+  createProduct: async (request, response) => {
+    try {
+      console.log(request.body);
+      const { name, price } = request.body;
+      const setData = {
+        name,
+        price,
+      };
+
+      const result = await productModel.createProduct(setData);
+
+      return wrapper.response(
+        response,
+        result.status,
+        "Success Create Data",
+        result.data
+      );
+    } catch (error) {
+      const {
+        status = 500,
+        statusText = "Internal Server Error",
+        error: errorData = null,
+      } = error;
+      return wrapper.response(response, status, statusText, errorData);
     }
   },
 };
+
+// request.query = bisa digunakan untuk fitur paginasi, sort,search di method get
+// request.params = bisa digunakan untuk fitur getdatabyid, updatedata, deletedata
+// request.body = bsa digunakan untuk fitur create/update
