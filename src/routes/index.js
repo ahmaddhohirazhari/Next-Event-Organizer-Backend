9b458ceeab832e71991f4416b67f0e6b1e597abe
ahmad dhohir azhari <dhohir12@gmail.com>
2022-09-18 23:04:44 +0700

    delete module product

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,14 +2,12 @@ const express = require("express");
 
 const Router = express.Router();
 
-const productRoutes = require("./product");
 const eventRoutes = require("./event");
 const userRoutes = require("./user");
 const wishlistRoutes = require("./wishlist");
 const bookingRoutes = require("./booking");
 const authRouters = require("./auth");
 
-Router.use("/product", productRoutes);
 Router.use("/event", eventRoutes);
 Router.use("/user", userRoutes);
 Router.use("/wishlist", wishlistRoutes);
