5740840316e7a1cc8e855b9356f3e84aa98fd035
ahmad dhohir azhari <dhohir12@gmail.com>
2022-09-19 07:59:24 +0700

    try

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,6 +1,6 @@
 /* eslint-disable prefer-destructuring */
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcryptjs");
+const bcrypt = require("bcrypt");
 const authModel = require("../models/auth");
 const wrapper = require("../utils/wrapper");
 const client = require("../config/redis");
