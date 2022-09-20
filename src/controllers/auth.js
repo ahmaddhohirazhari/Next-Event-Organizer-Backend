c5161b9ad25cd80ded9c12f3f4d91f4b2b3e8edd
ahmad dhohir azhari <dhohir12@gmail.com>
2022-09-20 11:56:26 +0700

    add nodmailer

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,9 +1,10 @@
-/* eslint-disable prefer-destructuring */
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
+const otpGenerator = require("otp-generator");
 const authModel = require("../models/auth");
 const wrapper = require("../utils/wrapper");
 const client = require("../config/redis");
+const { sendMail } = require("../utils/mail");
 
 module.exports = {
   register: async (request, response) => {
@@ -32,19 +33,55 @@ module.exports = {
       // PROSES PENGECEKAN APAKAH EMAIL YANG MAU DI DAFTARKAN SUDAH ADA ATAU BELUM ?
       const checkEmail = await authModel.getUserByEmail(email);
       if (checkEmail.data.length > 0) {
-        return wrapper.response(response, 404, "Email Alredy Registered", null);
+        return wrapper.response(response, 403, "Email Alredy Registered", null);
       }
 
       // PROSES MENYIMPAN DATA KE DATABASE LEWAT MODEL
       const result = await authModel.register(setData);
       const newResult = [{ userId: result.data[0].userId }];
 
+      // GENERATE OTP
+      const OTP = otpGenerator.generate(6, {
+        upperCaseAlphabets: false,
+        specialChars: false,
+        lowerCaseAlphabets: false,
+      });
+      client.setEx(`OTP:${OTP}`, 120, OTP);
+      // SEND EMAIL ACTIVATION
+      const setMailOptions = {
+        to: email,
+        name: username,
+        subject: "Email Verification !",
+        template: "verificationEmail.html",
+        buttonUrl: `http://localhost:3001/api/auth/verify/`,
+        OTP,
+      };
+
+      await sendMail(setMailOptions);
+
       return wrapper.response(
         response,
-        result.status,
-        "Successfully Registered",
+        200,
+        "Success Register Please Check Your Email",
         newResult
       );
+    } catch (error) {
+      const {
+        status = 500,
+        statusText = "Internal Server Error",
+        error: errorData = null,
+      } = error;
+      return wrapper.response(response, status, statusText, errorData);
+    }
+  },
+  verify: async (request, response) => {
+    try {
+      const { OTP } = request.body;
+      const cehckOTP = await client.get(`OTP:${OTP}`);
+      if (!cehckOTP) {
+        return wrapper.response(response, 400, "Wrong Input OTP", null);
+      }
+      return wrapper.response(response, 200, "Verify Success ", null);
     } catch (error) {
       const {
         status = 500,
@@ -111,7 +148,7 @@ module.exports = {
     try {
       let token = request.headers.authorization;
       const { refreshtoken } = request.headers;
-      token = token.split(" ")[1];
+      [token] = [token.split(" ")[1]];
 
       client.setEx(`accessToken:${token}`, 3600 * 48, token);
       client.setEx(`refreshoken:${refreshtoken}`, 3600 * 48, refreshtoken);
